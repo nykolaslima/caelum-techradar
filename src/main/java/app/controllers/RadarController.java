74601a22480482f721a017cf42ef3573ca2c558f
Mauricio Aniche <mauricioaniche@gmail.com>
2013-09-06 16:13:26 -0300

    colocando data

--- src/main/java/app/controllers/RadarController.java
+++ src/main/java/app/controllers/RadarController.java
@@ -2,11 +2,23 @@ package app.controllers;
 
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
 
 @Resource
 public class RadarController {
 
+	private Result result;
+
+	public RadarController(Result result) {
+		this.result = result;
+	}
+	
 	@Get("/")
 	public void radar() {
+		result.redirectTo(this).radarSet2013();
+	}
+	
+	@Get({"/setembro/2013", "/1"})
+	public void radarSet2013() {
 	}
 }
